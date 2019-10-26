0cd097477866623696c123d9e0b9ca520791ccd5
jameslindsaycs <j.lindsay1@nuigalway.ie>
2019-10-26 22:19:18 +0100

    added more unit tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,48 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+
   # test "the truth" do
   #   assert true
   # end
+
+  test "product attributes must not be empty" do
+  product = Product.new
+  assert product.invalid?
+  assert product.errors[:title].any?
+  assert product.errors[:description].any?
+  assert product.errors[:price].any?
+  assert product.errors[:image_url].any?
+end
+
+test "product price must be positive" do
+  product = products(:one)
+
+  product.price = -1
+  assert product.invalid?
+  assert product.errors[:price].any?
+
+  product.price = 0
+  assert product.invalid?
+  assert product.errors[:price].any?
+
+  product.price = 1
+  assert product.valid?
+  assert product.errors[:price].none?
+end
+
+test "product is not valid without a unique title" do
+  product = Product.new(:title       => products(:ruby).title,
+                        :description => "xxx",
+                        :price       => 1,
+                        :image_url   => "barney.gif")
+
+  assert product.invalid?
+  assert_equal ["has already been taken"], product.errors[:title]
+end
+
+
+
+
+
 end
